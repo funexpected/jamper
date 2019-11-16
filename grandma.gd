b9e269ae27a6d33ef998fa7594083fafb6724056
Andreev Andrei <andreevandreiv@gmail.com>
2019-11-16 20:08:34 +0300

    [grandma] add some animations

---
+++ grandma.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+export var texture = Color("4a57c3") setget set_texture
+
+func set_texture(value):
+	texture = value
+	check()
+
+func check():
+	pass
+
+var is_busy = false
+
+onready var obj = $back
+onready var sprite = $sprite
+func jump(prepare = true):
+	if !is_busy:
+		is_busy = true
+#		sprite.play("jump")
+		yield(tw.ip(obj, "rect_position:y", obj.rect_position.y, obj.rect_position.y - 150, 0.3), "tween_completed")
+		yield(tw.ip(obj, "rect_position:y", obj.rect_position.y, obj.rect_position.y + 150, 0.3), "tween_completed")
+		is_busy = false
