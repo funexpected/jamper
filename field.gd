7cb0826bc36a911b1c2c1d40ac1b98b9d7ac9a6f
Anna Gobruseva <nevanna@ya.ru>
2019-11-16 23:39:39 +0300

    del Image

--- field.gd
+++ field.gd
@@ -3,69 +3,49 @@ extends Control
 var png_path = "res://jumpers/%s.png"
 var a = 0.3
 onready var png = $field/png
+onready var jumper = $field/png/jumper
 var table = []
 var pic_size = Vector2(7,7)
 onready var save = $save
 var size_block= 150
 
 var image
-var image_table
 
 signal win
 signal success
 signal on_place
 
+
+
+onready var btn = $Button
+
 func _ready():
 	prepare_png()
 	create_table()
-	
+#	btn.connect("pressed", self, "exper")
 	print(table)
+
+func exper():
+	pass
 
 func prepare_png():
 	get_picture()
 	png.modulate.a = a
-
 
 func get_picture():
 	randomize()
 	var nb = randi() % 2
 	var img_path = png_path % nb
 	var img  = load(img_path)
-	
 	png.texture = img
 	print("COLOR")
 	image = png.texture.get_data()
 	image.lock()
-#	print(image)
-#	print(image.get_height())
-#	image.set_pixel(150, 150, Color(1, 0, 0, 1)) # Works
-	
 	var col = image.get_pixel(1, 1)
+	print(col)
 	var col_str = col.to_html(false)
 	print(col_str)
-	create_image_table(Vector2(image.get_height(), image.get_width()))
-	
-	
 
-#	var im = Image.new()
-#
-#	var tex = im.load(img_path).get_data()
-#	print(tex)
-#	var tex2 = im.create_from_data(7,7,false,Image.FORMAT_RGBA8, tex)
-#	print(tex2)
-##	im.create(300, 300, false, Image.FORMAT_RGBA8)
-#	im.lock()
-##	im.set_pixel(150, 150, Color(1, 0, 0, 1)) # Works
-##	im.unlock()
-##	img.set_pixel(x, y, color) # Does not have an effect
-#	print(im.get_pixel(1, 1))
-
-func create_image_table(size_tab):
-	image_table = Image.new()
-	image_table.create(size_tab.x, size_tab.y, false,  Image.FORMAT_RGBA8)
-	image_table.lock()
-	print("EMPTY")
-	print(image_table.get_pixel(1, 1))
 
 func create_table():
 	var plug = -1
@@ -80,16 +60,11 @@ func create_table():
 		i += 1
 
 func push(obj):
-	
-	var new_node = ColorRect.new()
-	new_node.rect_size = Vector2(150,150)
-	new_node.rect_position = obj.position
-	new_node.color = obj.color
 	var speed = obj.speed.y
-	save.add_child(new_node)
+#	save.add_child(new_node)
 	var dic = block_is_good(obj)
 	if dic  == TYPE_DICTIONARY:
-		run_block(obj, dic, new_node)
+		run_block(obj, dic)
 		return true
 	else:
 		return false
@@ -100,28 +75,27 @@ func block_is_good(obj):
 #	var column = obj.position.x / size_block
 	var column = tmp_obj_pos_x / size_block 
 	var cell = check_table(column)
-	var cell_image = check_image_table(column)
-	print("TWO_TABLES")
-	if (cell == cell_image):
-		print("true")
-	else: 
-		print("false")
 	if cell != Vector2(-1, -1):
 		return(__is_color_same(cell, obj))
 	return false
-
+	
+func is_full_jumper(table):
+	var i = int(0)
+	while i < table.y.size():
+		var j = int(0)
+		while j < table.x.size():
+			if table[i][j] < 0:
+				return false
+			j += 1
+		i += 1
+	emit_signal("win")
+	return true
+	
 func check_table(column):
 	var i = int(0)
+	var count = int(0)
 	while i < pic_size.y:
 		if table[column][i] == -1:
-			return Vector2(column, i)
-		i += 1
-	return Vector2(-1,-1)
-
-func check_image_table(column):
-	var i = int(0)
-	while i < image_table.get_height():
-		if image_table.get_pixel(column,i) == Color(0,0,0,0):
 			return Vector2(column, i)
 		i += 1
 	return Vector2(-1,-1)
@@ -138,7 +112,6 @@ func __is_color_same(cell, obj):
 	var _color_original = image.get_pixel(cell.x, cell.y).to_html()
 	var _color_from_obj = obj.color.to_html()
 	if _color_from_obj == _color_original:
-		image_table.set_pixel(cell.x, cell.y, _color_from_obj)
 		table[cell.x][cell.y] = Color(_color_from_obj)
 		dic["bool"] = true
 		dic["cell"]["x"] = cell.x
@@ -147,13 +120,17 @@ func __is_color_same(cell, obj):
 		dic["bool"] = false
 	return dic
 
-func run_block(obj, dic, new_node):
-	var s = get_distance(obj, dic, new_node)
+func run_block(obj, dic):
+	var s = get_distance(obj, dic)
 	
 	pass
 
-func get_distance(obj, dic, new_node):
+func get_distance(obj, dic):
+	yield(Time.defer(), "completed")
 	var start_pos = obj.position
-	var end_pos = Vector2(dic["cell"]["x"]*150, dic["cell"]["y"]*150)
-#	var s = 
-	
+	var end_pos = Vector2(start_pos.x, -1 * dic["cell"]["y"]*150 + 120)
+	var s = start_pos.y - end_pos.y
+	var t = s / obj.speed.y
+#	yield(tw.ip(new_node, "rect_position:y", start_pos.y, end_pos.y, t), "tween_completed")
+	yield(tw.ip(obj, "position:y", start_pos.y, end_pos.y, t), "tween_completed")
+	emit_signal("on_place")
