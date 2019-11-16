7cb1b251c2c0d3b7f91789bad45b67d7d1597c4d
Andreev Andrei <andreevandreiv@gmail.com>
2019-11-16 21:52:15 +0300

    [core] little fix to check ticks on bullet

--- level.gd
+++ level.gd
@@ -19,26 +19,34 @@ func _process(delta):
 	time += delta
 	if tick*DT < time: # need to update
 		tick += 1
-		if tick % 2 == 0:
-			print(tick/2)
-#		print(is_need_to_jump)
+		var bmb = ((tick)%28)*0.5-7
+		if bmb == 14:
+			$bullet.rect_position.x = -150*DT*(bmb-1)*4
+		print("%s, %s"%[tick*0.5, bmb])
+		grandma.get_node("back/text").text = str(bmb+4)
+		$bullet/text.text = str(bmb+4)
+		tw.ip($bullet, "rect_position:x", 150*DT*(bmb-1)*4, 150*DT*bmb*4, DT*2)
+		
 		if is_need_to_jump:
 			is_need_to_jump = false
 			grandma.jump()
 	if DEBUG:
 		print("time:%.2f > TICK[%003d]" % [time, tick])
 
-func _ready():
-	while true:
-		yield(tw.ip($bullet, "rect_position:x", -690, 690, 0.3*9), "tween_completed")
+#func _ready():
+#	while true:
+#		$bullet.rect_position.x = -825
+#		for i in 22:
+#			$bullet/text.text = str((i*0.5)-2)
+#			yield(tw.ip($bullet, "rect_position:x", $bullet.rect_position.x, $bullet.rect_position.x + 75, DT*2), "tween_completed")
 
 func _draw():
-	draw_line(Vector2(0, 0), Vector2(0, -1920), Color("80ffffff"), 1, true)
+	draw_line(Vector2(0, 120), Vector2(0, -1800), Color("80ffffff"), 1, true)
 	for k in 1920/75:
 		for i in 1080/150:
 			for j in 2:
 				var x = 75+i*75
 				x *= 1 if j else -1
-				draw_line(Vector2(x,0), Vector2(x, -1920), Color("30ffffff") if i % 2 else Color("90ffffff"), (1 if i % 2 else 3), true)
-		var y = -120 -75*k
-		draw_line(Vector2(-620,y), Vector2(620, y), Color("30ffffff") if k % 2 else Color("90ffffff"), (1 if k % 2 else 3), true)# if i % 2 else Color("ffffff"), (1 if i % 2 else 3), true)
+				draw_line(Vector2(x,120), Vector2(x, -1800), Color("30ffffff") if i % 2 else Color("90ffffff"), (1 if i % 2 else 3), true)
+		var y = -75*k
+		draw_line(Vector2(-620,y), Vector2(620, y), Color("30ffffff") if k % 2 else Color("90ffffff"), (1 if k % 2 else 3), true)
