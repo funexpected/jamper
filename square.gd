7d69f4fd220f840de96b8f2c5a49175af5abcad7
Yakov Borevich <j.borevich@gmail.com>
2019-11-17 13:43:47 +0300

    prepare layout for square.get_pos()

--- square.gd
+++ square.gd
@@ -5,8 +5,17 @@ const SIZE = Vector2(-150, -150)
 
 export var color = Color(1,1,1)
 
+var pos = Vector2(0,0) setget ,get_pos
+
+func get_pos():
+	pass
+
 func _ready():
-	pass # Replace with function body.
+	Time.connect("tick", self, "update_pos_text")
+	update_pos_text()
+	
+func update_pos_text(tick=0):
+	$label.text = "%0.1f, %0.1f" % [pos.x, pos.y]
 
 func _draw():
 	draw_rect(Rect2(-SIZE*0.5, SIZE), color, true)
